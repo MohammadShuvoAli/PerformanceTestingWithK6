858292360b54b5038d18565ee08810f3f72b10e9
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-06-06 22:45:13 +0600

    Update Hello.js

--- HelloWorld/Hello.js
+++ HelloWorld/Hello.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import {sleep} from 'k6';
 
 export const options = {
-    vus: 10,
-    duration: '5s',
+    vus: 100,
+    duration: '10s',
 };
 
 export default function () {
