eaba0ddc2ff9256f8b4a4a8b603259f21f365971
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-04 04:34:38 +0600

    update

--- Presentation/Report.js
+++ Presentation/Report.js
@@ -3,43 +3,16 @@ import { check } from "k6";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export let options = {
-    batch: 5, // default: 20
-    batchPerHost: 5, // default: 6
-  };
-  
-export default function() {
-    let req1 = {
-        method: "GET",
-        url: "https://google.com",
-    };
+    stages: [
+    { duration: "5s", target: 20 }, // ramp up
+    { duration: "15s", target: 20 },
+    { duration: "5s", target: 0 } // ramp down
+    ],
+   };
 
-    let req2 = {
-        method: "GET",
-        url: "https://duckduckgo.com",
-    };
-
-    let res = http.batch([req1, req2]);
-
-    check(res[0], {
-        'Google.com was 200': (res) => res.status === 200,
-    });
-
-    check(res[1], {
-        'DuckDuckGo.com was 200': (res) => res.status === 200,
-    });
-
-    // Named requests
-    let namedRequests = {
-        'Home page': 'https://k6.io',
-        'Pricing page': {
-          method: 'GET',
-          url: 'https://k6.io/pricing'
-        },
-    };
-
-    let namedResponses = http.batch(namedRequests);
-    console.log("Home Page response " + namedResponses['Home page'].status);
-    console.log("Pricing Page response " + namedResponses['Pricing page'].status);
+export default function () {
+    http.get('https://sjinnovation.com');
+    sleep(2);
 }
 
 export function handleSummary(data) {
